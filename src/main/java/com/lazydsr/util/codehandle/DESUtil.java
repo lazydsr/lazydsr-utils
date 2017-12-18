65faadddd39a9c5660a2b220a4b39d0c5fb96cc4
代森荣 <168367406@qq.com>
2017-12-18 23:26:49 +0800

    添加des工具类

---
+++ src/main/java/com/lazydsr/util/codehandle/DESUtil.java
@@ -0,0 +1,115 @@
+package com.lazydsr.util.codehandle;
+
+/**
+ * DESUtil
+ * PROJECT_NAME: DESUtil
+ * PACKAGE_NAME: com.lazydsr.util.codehandle
+ * Created by Lazy on 2017/4/13 13:30
+ * Version: 1.0
+ * Info: 对称加密算法工具类
+ */
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+public class DESUtil {
+
+    private final static String DES = "DES";
+    private final static String ENCODE = "UTF-8";
+
+
+    /**
+     * 根据key对密文data进行加密
+     *
+     * @param data 密文
+     * @param key  密钥
+     * @return 密文字符串
+     * @throws Exception
+     */
+    public static String encrypt(String data, String key) throws Exception {
+        byte[] bt = encrypt(data.getBytes(ENCODE), key.getBytes(ENCODE));
+        String strs = new BASE64Encoder().encode(bt);
+        return strs;
+    }
+
+    /**
+     * 根据key对密文data进行解密
+     *
+     * @param data 密文
+     * @param key  密钥
+     * @return 明文字符串
+     * @throws Exception
+     */
+    public static String decrypt(String data, String key) throws IOException,
+            Exception {
+        if (data == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buf = decoder.decodeBuffer(data);
+        byte[] bt = decrypt(buf, key.getBytes(ENCODE));
+        return new String(bt, ENCODE);
+    }
+
+    /**
+     * 根据key对密文data进行加密
+     *
+     * @param data 明文数组
+     * @param key  密钥数组
+     * @return 密文数组
+     * @throws Exception
+     */
+    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance(DES);
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * 根据key对密文data进行解密
+     *
+     * @param data 密文数组
+     * @param key  密钥数组
+     * @return 明文数组
+     * @throws Exception
+     */
+    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成解密操作
+        Cipher cipher = Cipher.getInstance(DES);
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+}
---
+++ src/test/java/com/lazydsrtest/code/DesUtil.java
@@ -0,0 +1,24 @@
+package com.lazydsrtest.code;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+/**
+ * DesUtil
+ * PROJECT_NAME: DESUtil
+ * PACKAGE_NAME: com.lazydsr.utils
+ * Created by Lazy on 2017/12/18 21:30
+ * Version: 1.0
+ * Info: 对称加密算法工具类
+ */
+public class DesUtil {
+
+
+}
